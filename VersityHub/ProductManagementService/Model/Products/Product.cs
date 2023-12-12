e326cdf297bcbc28265354ac58aed3c15ca2a678
Ifedayo Awoniyi <iawoniyi@lagetronix.com>
2023-12-12 13:52:25 +0100

    Add feature: Updated Product entities

--- VersityHub/ProductManagementService/Model/Products/Product.cs
+++ VersityHub/ProductManagementService/Model/Products/Product.cs
@@ -10,5 +10,6 @@ namespace ProductManagementService.Model.Products
         public string Name { get; set; }
         public string Description { get; set; }
         public decimal UnitPrice { get; set; }
+        public long StockQuantity { get; set; }
     }
 }
